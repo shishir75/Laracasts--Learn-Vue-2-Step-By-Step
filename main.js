a56205ebee5a8d477e16ec646109102f52a14a96
shishir75 <shishir.srdr16@gmail.com>
2020-02-15 00:41:19 +0600

    Component Communication Exam 1: Custom Events

--- main.js
+++ main.js
@@ -33,8 +33,36 @@ Vue.component('task-list', {
 });
 
 
+// custom event
+
+Vue.component('coupon', {
+
+    template: '<input type="text" placeholder="Enter your coupon code" @blur="onCouponApplied">',
+
+
+    methods: {
+        onCouponApplied() {
+
+            this.$emit('applied');
+        }
+    }
+
+
+});
+
+
 new Vue({
     el: '#app',
+
+    data: {
+        couponApplied: false,
+    },
+
+    methods: {
+        onCouponApplied() {
+            this.couponApplied = true;
+        }
+    }
 
 
 });
