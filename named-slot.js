33f4a200ec172d231cc52d6ad275e83d7545d633
shishir75 <shishir.srdr16@gmail.com>
2020-02-15 01:53:58 +0600

    Component Named Slot in a Nutshell

---
+++ named-slot.js
@@ -0,0 +1,51 @@
+Vue.component('model', {
+
+   template: `
+   
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+          
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">
+                    
+                        <slot name="header"></slot>
+                        
+                    </p>
+                    <button class="delete" aria-label="close"></button>
+                </header>
+            
+                <section class="modal-card-body">
+                
+                    <slot>
+                        Default content here.....
+                   
+                    </slot>
+                </section>
+            
+                <footer class="modal-card-foot">
+                
+                    <slot name="footer">
+                    
+                        <button class="button is-success">Okay (Default)</button> 
+                        
+                    </slot>
+                    
+                </footer>
+            
+            </div>
+        </div>
+   
+   `,
+
+
+
+});
+
+
+new Vue({
+
+    el: '#app',
+
+
+});
